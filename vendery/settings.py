d964a50d8053a265c2b7ddc7389a708ca2f02b40
laloh <eduardocarhue@gmail.com>
2021-08-15 01:47:49 -0500

    clear

--- vendery/settings.py
+++ vendery/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,85 +24,85 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9um+!0gl@dp46^aqqfn3ayfps*q)z#w%s6=7^4q5pc9#cy^%l='
+SECRET_KEY = "9um+!0gl@dp46^aqqfn3ayfps*q)z#w%s6=7^4q5pc9#cy^%l="
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if env('IS_LOCAL') else False
 
-ALLOWED_HOSTS = ['www.vendery.app','vendery.app','*', 'localhost']
+ALLOWED_HOSTS = ["www.vendery.app", "vendery.app", "*", "localhost"]
 
 
 # Application definition
 SHARED_APPS = (
-    'django_tenants',  # mandatory
-    'vendery.customers', # you must list the app where your tenant model resides in
-    'vendery.admin_panel', #
-
-    'django.contrib.contenttypes',
-
+    "django_tenants",  # mandatory
+    "vendery.customers",  # you must list the app where your tenant model resides in
+    "vendery.admin_panel",  #
+    "django.contrib.contenttypes",
     # everything below here is optional
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'jet',
-    'django.contrib.admin',
-    'django.contrib.staticfiles',
-    'django_extensions',
-    'widget_tweaks'
-
+    "django.contrib.auth",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "jet",
+    "django.contrib.admin",
+    "django.contrib.staticfiles",
+    "django_extensions",
+    "widget_tweaks",
 )
 
 TENANT_APPS = (
     # The following Django contrib apps must be in TENANT_APPS
-    'django.contrib.contenttypes',
-
+    "django.contrib.contenttypes",
     # your tenant-specific apps
-    'vendery.inventory',
-    'vendery.admin_panel',
+    "vendery.inventory",
+    "vendery.admin_panel",
     # everything below here is optional
-    'django.contrib.auth',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'jet',
-    'django.contrib.admin',
-    'django.contrib.staticfiles',
-    'django_extensions'
+    "django.contrib.auth",
+    "django.contrib.sessions",
+    "django.contrib.sites",
+    "django.contrib.messages",
+    "jet",
+    "django.contrib.admin",
+    "django.contrib.staticfiles",
+    "django_extensions",
 )
 
-X_FRAME_OPTIONS='SAMEORIGIN' # only if django version >= 3.0
+X_FRAME_OPTIONS = "SAMEORIGIN"  # only if django version >= 3.0
 
-INSTALLED_APPS = list(SHARED_APPS) + [app for app in TENANT_APPS if app not in SHARED_APPS]
-
-MIDDLEWARE = [
-    'django_tenants.middleware.main.TenantMainMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+INSTALLED_APPS = list(SHARED_APPS) + [
+    app for app in TENANT_APPS if app not in SHARED_APPS
 ]
 
-PUBLIC_SCHEMA_URLCONF = 'vendery.public_urls'
-ROOT_URLCONF = 'vendery.urls'
-APPEND_SLASH=True
+MIDDLEWARE = [
+    "django_tenants.middleware.main.TenantMainMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    # 'django.middleware.csrf.CsrfViewMiddleware',
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+PUBLIC_SCHEMA_URLCONF = "vendery.public_urls"
+ROOT_URLCONF = "vendery.urls"
+APPEND_SLASH = True
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        "DIRS": [f"{BASE_DIR}/templates"],  # -> Dirs used by the standard template loader
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            f"{BASE_DIR}/templates"
+        ],  # -> Dirs used by the standard template loader
         # 'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
             "loaders": [
                 "django_tenants.template.loaders.filesystem.Loader",  # Must be first
@@ -109,46 +113,42 @@ TEMPLATES = [
     },
 ]
 
-MULTITENANT_TEMPLATE_DIRS = [
-    f"{BASE_DIR}/vendery/tenants/%s/templates"
-]
+MULTITENANT_TEMPLATE_DIRS = [f"{BASE_DIR}/vendery/tenants/%s/templates"]
 
-WSGI_APPLICATION = 'vendery.wsgi.application'
+WSGI_APPLICATION = "vendery.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django_tenants.postgresql_backend',
-        'NAME': 'vendery',
-        'USER': 'vendery',
-        'PASSWORD': 'test1234',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django_tenants.postgresql_backend",
+        "NAME": "vendery",
+        "USER": "vendery",
+        "PASSWORD": "test1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
-DATABASE_ROUTERS = (
-    'django_tenants.routers.TenantSyncRouter',
-)
+DATABASE_ROUTERS = ("django_tenants.routers.TenantSyncRouter",)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -157,9 +157,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'es'
+LANGUAGE_CODE = "es"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -176,7 +176,7 @@ STATICFILES_FINDERS = [
     "django_tenants.staticfiles.finders.TenantFileSystemFinder",  # Must be first
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    'compressor.finders.CompressorFinder',
+    "compressor.finders.CompressorFinder",
 ]
 
 MULTITENANT_STATICFILES_DIRS = [
@@ -187,11 +187,11 @@ MULTITENANT_STATICFILES_DIRS = [
 STATICFILES_STORAGE = "django_tenants.staticfiles.storage.TenantStaticFilesStorage"
 REWRITE_STATIC_URLS = True
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = "/staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 MULTITENANT_RELATIVE_STATIC_ROOT = "tenants/%s"
 
-TENANT_MODEL = "customers.Client" # app.Model
+TENANT_MODEL = "customers.Client"  # app.Model
 TENANT_DOMAIN_MODEL = "customers.Domain"  # app.Model
 SITE_ID = 1
 
@@ -203,23 +203,29 @@ MULTITENANT_RELATIVE_MEDIA_ROOT = "%s/other_dir"
 
 JET_SIDE_MENU_COMPACT = True
 JET_SIDE_MENU_ITEMS = [
-    {'app_label': 'inventory', 'items': [
-        {'name': 'category', 'label': 'Categorias'},
-        {'name': 'clients', 'label': 'Clientes'},
-        {'name': 'products', 'label': 'Productos'},
-        {'name': 'tickets', 'label': 'Tickets'},
-        {'name': 'vendors', 'label': 'Vendedores'},
-        {'name': 'orders', 'label': 'Ventas'},
-    ]},
-    {'app_label': 'sites', 'items': [
-        {'name': 'site', 'label': 'Páginas'},
-    ]},
+    {
+        "app_label": "inventory",
+        "items": [
+            {"name": "category", "label": "Categorias"},
+            {"name": "clients", "label": "Clientes"},
+            {"name": "products", "label": "Productos"},
+            {"name": "tickets", "label": "Tickets"},
+            {"name": "vendors", "label": "Vendedores"},
+            {"name": "orders", "label": "Ventas"},
+        ],
+    },
+    {
+        "app_label": "sites",
+        "items": [
+            {"name": "site", "label": "Páginas"},
+        ],
+    },
 ]
 
 # Image Resize
 DJANGORESIZED_DEFAULT_SIZE = [300, 250]
 DJANGORESIZED_DEFAULT_QUALITY = 75
 DJANGORESIZED_DEFAULT_KEEP_META = True
-DJANGORESIZED_DEFAULT_FORCE_FORMAT = 'JPEG'
-DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {'JPEG': ".jpg"}
+DJANGORESIZED_DEFAULT_FORCE_FORMAT = "JPEG"
+DJANGORESIZED_DEFAULT_FORMAT_EXTENSIONS = {"JPEG": ".jpg"}
 DJANGORESIZED_DEFAULT_NORMALIZE_ROTATION = True
